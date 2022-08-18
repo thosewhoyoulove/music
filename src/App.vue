fed807f9d3b5ee25aad6a745690e1c654c0d82b9
thosewhoyoulove <2406001659@qq.com>
2022-08-18 19:11:46 +0800

    完成头部导航和轮播图的功能

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
+<!--
+ * @Description: 
+ * @Author: 曹俊
+ * @Date: 2022-08-18 17:12:27
+ * @LastEditors: 曹俊
+ * @LastEditTime: 2022-08-18 17:51:14
+-->
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+  <main font-sans p=".5rem" text="center gray-700 dark:gray-200">
     <router-view />
-    <Footer />
+    <TopNav />
+    <Swiper/>
   </main>
 </template>
+<style lang="less">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.icon {
+  width: 2rem;
+  height: 2rem;
+}
+</style>
---
+++ src/components/Swiper.vue
@@ -0,0 +1,42 @@
+<!--
+ * @Description: 
+ * @Author: 曹俊
+ * @Date: 2022-08-18 17:49:56
+ * @LastEditors: 曹俊
+ * @LastEditTime: 2022-08-18 19:05:47
+-->
+<template>
+  <div class="pt-2">
+    <van-swipe class="w-100% " :autoplay="3000" lazy-render>
+      <van-swipe-item class="w-100% h-40" v-for="image in state.image" :key="image">
+        <img class="h-30 w-100% h-100% " :src="image.pic" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {reactive, onMounted} from 'vue'
+import {getBanner} from '~/api/banner.js'
+
+const state = reactive({
+    image:[
+        
+]})
+onMounted(async () => {
+    let res = await getBanner()
+    state.image = res.data.banners
+    
+    
+})
+</script>
+
+<style scoped>
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+  background-color: #da1717;
+}
+</style>
