b8e37bea76d70ee4ca69a3dea0270e80210e7ec6
thosewhoyoulove <2406001659@qq.com>
2022-08-23 18:09:09 +0800

    列表样式

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@
  * @Author: 曹俊
  * @Date: 2022-08-17 15:49:05
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-08-18 18:50:50
+ * @LastEditTime: 2022-08-23 17:30:18
  */
 import axios from 'axios'
 // 创建axios实例
@@ -11,7 +11,7 @@ const service = axios.create({
     // 请求路由
     baseURL: 'https://netease-cloud-music-9aqed2u7k-thosewhoyoulove.vercel.app',
     // 请求超时时间
-    timeout: 3000,
+    timeout: 10000,
     withCredentials: true,
 })
 
