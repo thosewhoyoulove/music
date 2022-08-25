13c7435df4d40bd97b4164de178f48427d000a98
thosewhoyoulove <2406001659@qq.com>
2022-08-25 22:21:59 +0800

    歌曲评论

--- src/api/SongDetail.js
+++ src/api/SongDetail.js
@@ -3,7 +3,7 @@
  * @Author: 曹俊
  * @Date: 2022-08-24 18:35:26
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-08-24 18:36:36
+ * @LastEditTime: 2022-08-25 13:00:00
  */
 import request from '~/utils/request'
 
@@ -13,5 +13,12 @@ export function getMusicLyric(data) {
     return request({
         method: 'get',
         url: `/lyric?id=${data}`
+    })
+}
+
+export function getMusicComment(data) {
+    return request({
+        method: 'get',
+        url: `/comment/music?id=${data}&limit=2`
     })
 }
--- src/pages/SongComment.vue
+++ src/pages/SongComment.vue
@@ -3,7 +3,7 @@
  * @Author: 曹俊
  * @Date: 2022-08-25 12:42:09
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-08-25 12:47:02
+ * @LastEditTime: 2022-08-25 13:02:00
 -->
 <template>
     <div>
@@ -12,10 +12,13 @@
 </template>
 
 <script setup lang="ts">
+import { getMusicComment } from "~/api/SongDetail";
 const route = useRoute()
 const id = route.query.id
-onMounted(() => {
-    console.log(id)
+onMounted(async () => {
+    let res = await getMusicComment(id)
+    console.log(res);
+    
 })
 </script>
 
