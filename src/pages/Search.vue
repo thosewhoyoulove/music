1cc6d7987d7a05695c9acda14101db3abdcdcbdf
thosewhoyoulove <2406001659@qq.com>
2022-08-28 11:51:37 +0800

    定义数组不要用reactive

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -3,33 +3,46 @@
  * @Author: 曹俊
  * @Date: 2022-08-27 11:27:10
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-08-28 10:14:05
+ * @LastEditTime: 2022-08-28 11:00:04
 -->
+<script setup lang="ts">
+import { getSearchMusic } from "~/api/Search"; 
+const  keyWordList = ref([])
+let  keyWord = ref('')
+const onSearch = () =>{
+        keyWordList.value.push(keyWord.value)
+        localStorage.setItem('keyWordList',JSON.stringify(keyWordList.value))
+        keyWord.value = ""
+        console.log(keyWordList.value,'数据数组');
+        
+}
+onMounted(() =>{
+    keyWordList.value = JSON.parse(localStorage.getItem('keyWordList')) || []
+})
+</script>
+
 <template>
-    <div>
+    <div class="w-100% h-100% bg-hex-eee p-1">
         <div>
             <van-search
-            v-model="value"
+            v-model="keyWord"
             show-action
             placeholder="奇妙能力歌 - 陈粒"
             @search="onSearch"
+            class="rounded"
             >
             <template #action>
-                <div @click="onClickButton">搜索</div>
+                <div @click="onSearch">搜索</div>
             </template>
         </van-search>
         </div>
-        <div class="text-left ml-2.5 flex">
-            <div>历史记录:</div>
-            <div v-for="(item,index) in keyWordList" :key="index"></div>
+        <div class="text-left ml-2.5">
+            <span class="text-sm font-700">历史记录:</span>
+            <span class="text-xs inline-block mt-1 mx-1 py-.2rem px-.6rem bg-white rounded-full border " v-for="(item,index) in keyWordList" :key="index">{{item}}</span>
         </div>
     </div>
 </template>
 
-<script setup lang="ts">
-import { getSearchMusic } from "~/api/Search"; 
-const keyWordList = reactive([])
-</script>
 
 <style scoped>
 
