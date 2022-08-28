e8fe459ec94fa3f3737873a11b08b6c283c98993
thosewhoyoulove <2406001659@qq.com>
2022-08-28 10:14:06 +0800

    搜索功能

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -3,11 +3,12 @@
  * @Author: 曹俊
  * @Date: 2022-08-27 11:27:10
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-08-27 12:40:01
+ * @LastEditTime: 2022-08-28 10:14:05
 -->
 <template>
     <div>
-        <van-search
+        <div>
+            <van-search
             v-model="value"
             show-action
             placeholder="奇妙能力歌 - 陈粒"
@@ -17,11 +18,17 @@
                 <div @click="onClickButton">搜索</div>
             </template>
         </van-search>
+        </div>
+        <div class="text-left ml-2.5 flex">
+            <div>历史记录:</div>
+            <div v-for="(item,index) in keyWordList" :key="index"></div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { getSearchMusic } from "~/api/Search"; 
+const keyWordList = reactive([])
 </script>
 
 <style scoped>
