dc8516c8d36b9b660ae3bbf007a0dfa79b524668
thosewhoyoulove <2406001659@qq.com>
2022-09-08 21:50:49 +0800

    为每日推荐写好基础结构

--- src/api/RecommendSongList.js
+++ src/api/RecommendSongList.js
@@ -3,13 +3,20 @@
  * @Author: 曹俊
  * @Date: 2022-08-17 15:53:02
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-08-18 20:56:09
+ * @LastEditTime: 2022-09-08 21:20:43
  */
 import request from '~/utils/request'
-
+//获取推荐歌曲，不需要登录
 export default function getRecommendSongList() {
     return request({
         method: 'get',
         url: '/personalized?limit=10',
+    })
+}
+//获取每日推荐歌曲，需要登录
+export function getDailyRecommendSongs() {
+    return request({
+        method: 'get',
+        url: '/recommend/songs',
     })
 }
--- src/pages/DailyRecommend.vue
+++ src/pages/DailyRecommend.vue
@@ -3,16 +3,30 @@
  * @Author: 曹俊
  * @Date: 2022-09-06 19:07:49
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-09-06 19:08:00
+ * @LastEditTime: 2022-09-08 21:21:55
 -->
 <template>
-    <div>
-        每日推荐
+    <div class="w-100% h-30vh relative">
+        <img class="w-100% h-100% rounded-xl absolute blur-xl" src="../public/Reco-bg.jpg" alt="背景图">
+        <div class="flex w-50 items-center text-blue-500 pt-34">
+            <div class="text-3xl font-500">{{Month}}月</div> / <div>{{Day}}日</div>
+        </div>
     </div>
+    <div class="w-100% mt-10">这个需要登录</div>
 </template>
 
 <script setup lang="ts">
-
+import { getDailyRecommendSongs } from '~/api/RecommendSongList'
+//获取时间
+const day = new Date()
+let Day = ref(day.getUTCDate())
+const month = new Date()
+let Month = ref(month.getMonth() + 1)
+// onMounted(async() => {
+//     let res = await getDailyRecommendSongs()
+//     console.log(res);
+    
+// })
 </script>
 
 <style scoped>
