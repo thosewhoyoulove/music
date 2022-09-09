932964c755f4bd5afde48c79985cea9d3730209d
thosewhoyoulove <2406001659@qq.com>
2022-09-09 19:50:34 +0800

    手机号，二维码登录

--- components.d.ts
+++ components.d.ts
@@ -23,6 +23,7 @@ declare module '@vue/runtime-core' {
     VanCellGroup: typeof import('vant/es')['CellGroup']
     VanDialog: typeof import('vant/es')['Dialog']
     VanField: typeof import('vant/es')['Field']
+    VanForm: typeof import('vant/es')['Form']
     VanIcon: typeof import('vant/es')['Icon']
     VanList: typeof import('vant/es')['List']
     VanLoading: typeof import('vant/es')['Loading']
