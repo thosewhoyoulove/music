a8cdb1645efee6b6916ab6cba60cea0ea134ef65
thosewhoyoulove <2406001659@qq.com>
2022-09-10 20:09:42 +0800

    注册的页面

---
+++ src/pages/Register.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Description: 
+ * @Author: 曹俊
+ * @Date: 2022-09-10 09:57:18
+ * @LastEditors: 曹俊
+ * @LastEditTime: 2022-09-10 14:03:51
+-->
+<template>
+    <div class="h-100vh w-99% bg-hex-DB2C1F">
+        111
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
