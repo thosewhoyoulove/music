cb484c6c0a8724424b84cb72dbdb181bb602b6fa
thosewhoyoulove <2406001659@qq.com>
2022-09-11 19:38:21 +0800

    增加了邮箱登录的选项

--- src/pages/LoginOrReg.vue
+++ src/pages/LoginOrReg.vue
@@ -3,7 +3,7 @@
  * @Author: 曹俊
  * @Date: 2022-09-10 14:01:20
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-09-10 16:18:47
+ * @LastEditTime: 2022-09-11 17:26:57
 -->
 <template>
   <div class="h-100vh w-99% bg-hex-DB2C1F relative">
@@ -29,13 +29,28 @@
         >
       </div>
     </div>
-    <div class="flex items-center text-white text-12px w-60 mt-4 left-50% top-85% absolute -translate-x-1/2">
+    <div
+      class="
+        flex
+        items-center
+        text-white text-12px
+        w-60
+        mt-4
+        left-50%
+        top-85%
+        absolute
+        -translate-x-1/2
+      "
+    >
       <div class="flex">
-        <van-checkbox  v-model="checked" checked-color="#ee0a24" icon-size="14px">同意</van-checkbox>
+        <van-checkbox v-model="checked" checked-color="#ee0a24" icon-size="14px"
+          >同意</van-checkbox
+        >
       </div>
       <div class="ml-2">服务条款 隐私政策 儿童隐私政策</div>
     </div>
     <img
+      @click="toEmailLogin"
       class="h-8 w-8 mt-5 left-50% top-89% absolute -translate-x-1/2"
       src="/email.png"
       alt=""
@@ -47,20 +62,27 @@
 import { useStore } from "~/store/index";
 import { storeToRefs } from "pinia";
 import { Notify } from "vant/es";
-const router = useRouter()
+const router = useRouter();
 const store = useStore();
 const { isFooterShow } = storeToRefs(store);
-const checked = ref(false)
+const checked = ref(false);
 onMounted(() => {
   isFooterShow.value = false;
 });
-const toLogin = ()=>{
-    if(!checked.value){
-        Notify({type:'warning',message:'请勾选同意条款和政策'})
-    }else{
-        router.push({path:'/login'})
-    }
-}
+const toLogin = () => {
+  if (!checked.value) {
+    Notify({ type: "warning", message: "请勾选同意条款和政策" });
+  } else {
+    router.push({ path: "/login" });
+  }
+};
+const toEmailLogin = () => {
+  if (!checked.value) {
+    Notify({ type: "warning", message: "请勾选同意条款和政策" });
+  } else {
+    router.push({ path: "/LoginByEmail" });
+  }
+};
 </script>
 
 <style scoped>
@@ -77,5 +99,4 @@ const toLogin = ()=>{
   border: 1px solid white;
   height: 2.5rem;
 }
-
 </style>
