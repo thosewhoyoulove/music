1d20e71524ddc53fd3764ef122839c72bd3dacbb
thosewhoyoulove <2406001659@qq.com>
2022-09-12 21:20:38 +0800

    增加获取用户信息的接口

--- src/api/user.js
+++ src/api/user.js
@@ -3,9 +3,12 @@
  * @Author: 曹俊
  * @Date: 2022-08-17 15:53:02
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-09-11 17:14:41
+ * @LastEditTime: 2022-09-12 20:47:22
  */
 import request from '~/utils/request'
+import { storeToRefs } from "pinia";
+import { useStore, userStore } from "~/store/index";
+
 // 获取用户详情
 // 说明 : 登录后调用此接口 , 传入用户 id, 可以获取用户详情
 
@@ -18,5 +21,21 @@ export function getUserDetail(uid) {
     return request({
         method: 'get',
         url: `/user/detail?uid=${uid}`,
+    })
+}
+// 获取用户信息 , 歌单，收藏，mv, dj 数量
+// 说明 : 登录后调用此接口 , 可以获取用户信息
+
+// 接口地址 : /user/subcount
+
+// 调用例子 : /user/subcount
+export function getUserAcount() {
+    const store = useStore();
+    const userInfo = userStore()
+    const { user } = storeToRefs(userInfo) //获得用户信息
+    return request({
+        method: 'post',
+        withCredentials: true,
+        url: `/user/subcount?cookie=${encodeURIComponent(user.value.cookie)}`,
     })
 }
