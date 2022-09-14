58f363bbe1411d1eef2f1cefeae057249d3e651d
thosewhoyoulove <2406001659@qq.com>
2022-09-14 21:32:34 +0800

    保存扫码登录的结果

--- src/api/user.js
+++ src/api/user.js
@@ -3,7 +3,7 @@
  * @Author: 曹俊
  * @Date: 2022-08-17 15:53:02
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-09-12 21:24:09
+ * @LastEditTime: 2022-09-13 21:43:27
  */
 import request from '~/utils/request'
 import { storeToRefs } from "pinia";
@@ -40,5 +40,30 @@ export function getUserAcount() {
         method: 'post',
         withCredentials: true,
         url: `/user/subcount?cookie=${encodeURIComponent(user.value.cookie)}`,
+    })
+}
+
+// 获取用户歌单
+// 说明 : 登录后调用此接口 , 传入用户 id, 可以获取用户歌单
+
+// 必选参数 : uid : 用户 id
+
+// 可选参数 :
+
+// limit : 返回数量 , 默认为 30
+
+// offset : 偏移数量，用于分页 , 如 :( 页数 -1)*30, 其中 30 为 limit 的值 , 默认为 0
+
+// 接口地址 : /user/playlist
+
+// 调用例子 : /user/playlist?uid=32953014
+
+export function getUserPlaylist(uid) {
+    const store = useStore();
+    const userInfo = userStore()
+    const { user } = storeToRefs(userInfo) //获得用户信息
+    return request({
+        method: 'post',
+        url: `/user/event?uid=${uid}`,
     })
 }
