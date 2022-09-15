0cf182fa76adc9ca0dcc153aaffa28e9577230e6
thosewhoyoulove <2406001659@qq.com>
2022-09-15 19:38:48 +0800

    增加了获取用户关注，粉丝的接口

--- src/api/user.js
+++ src/api/user.js
@@ -3,7 +3,7 @@
  * @Author: 曹俊
  * @Date: 2022-08-17 15:53:02
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-09-15 10:01:43
+ * @LastEditTime: 2022-09-15 15:54:59
  */
 import request from '~/utils/request'
 import { storeToRefs } from "pinia";
@@ -54,11 +54,50 @@ export function getUserAcount() {
 // 调用例子 : /user/playlist?uid=32953014
 
 export function getUserPlaylist(uid) {
-    const store = useStore();
-    const userInfo = userStore()
-    const { user } = storeToRefs(userInfo) //获得用户信息
     return request({
         method: 'post',
         url: `/user/playlist?uid=${uid}`,
+    })
+}
+
+// 获取用户关注列表
+// 说明 : 登录后调用此接口 , 传入用户 id, 可以获取用户关注列表
+
+// 必选参数 : uid : 用户 id
+
+// 可选参数 :
+
+// limit : 返回数量 , 默认为 30
+
+// offset : 偏移数量，用于分页 ,如 :( 页数 -1)*30, 其中 30 为 limit 的值 , 默认为 0
+
+// 接口地址 : /user/follows
+
+// 调用例子 : /user/follows?uid=32953014
+
+export function getUserFollows(uid) {
+    const cookie = localStorage.getItem('cookie')
+    return request({
+        method: 'post',
+        url: `/user/follows?uid=${uid}`,
+    })
+}
+// 获取用户粉丝列表
+// 说明 : 登录后调用此接口 , 传入用户 id, 可以获取用户粉丝列表
+
+// 必选参数 : uid : 用户 id
+
+// 可选参数 : limit : 返回数量 , 默认为 30
+
+// offset : 偏移数量，用于分页 ,如 :( 页数 -1)*30, 其中 30 为 limit 的值 , 默认为 0
+
+// 接口地址 : /user/followeds
+
+// 调用例子 : /user/followeds?uid=32953014 /user/followeds?uid=416608258&limit=1 /user/followeds?uid=416608258&limit=1&offset=1
+export function getUserFolloweds(uid) {
+    const cookie = localStorage.getItem('cookie')
+    return request({
+        method: 'post',
+        url: `/user/followeds?uid=${uid}`,
     })
 }
