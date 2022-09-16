864580f3a828a3b3560ffe1a2b5fb5d8d229071d
thosewhoyoulove <2406001659@qq.com>
2022-09-16 21:06:48 +0800

    完成了歌曲时间的实现

--- src/components/MusicDetail.vue
+++ src/components/MusicDetail.vue
@@ -3,7 +3,7 @@
  * @Author: 曹俊
  * @Date: 2022-08-22 21:03:00
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-09-16 20:09:50
+ * @LastEditTime: 2022-09-16 21:05:53
 -->
 <script setup lang="ts">
 import { Vue3Marquee } from "vue3-marquee";
@@ -19,6 +19,7 @@ const props = defineProps<{
 const router = useRouter();
 const store = useStore();
 const isLyricShow = ref(false); // 歌词是否显示
+
 const totalComment = ref(0);
 
 const { playList, playListIndex, currentTime, duration } = storeToRefs(store);
@@ -26,13 +27,21 @@ const { playList, playListIndex, currentTime, duration } = storeToRefs(store);
 const timeFilter = (time) => {
   const minRes = Math.floor(parseInt(time) / 60);
   const secRes = parseInt(time) % 60;
-  const res = `${minRes}:${secRes}`;
-  console.log(minRes, secRes);
+  let res;
+  if (minRes < 10) {
+    res = "0" + minRes + ":" + secRes;
+  }
+  if (secRes < 10) {
+    res = minRes + ":" + "0" + secRes;
+  } else {
+    res = `${minRes}:${secRes}`;
+  }
 
+  console.log(minRes, secRes);
   return res;
 };
-
-const totalTime = ref(timeFilter(duration.value));
+const nowTime = ref(timeFilter(store.currentTime)); //获取当前时间
+const totalTime = ref(timeFilter(duration.value)); //获取总时长
 console.log(totalTime, "totalTime");
 
 const { isShow, isDetailShow } = storeToRefs(store);
@@ -40,13 +49,17 @@ onMounted(async () => {
   const res = await getMusicComment(props.musicList.id);
   totalComment.value = res.total;
   console.log(totalComment.value, "音乐评论数");
+  totalTime.value = timeFilter(duration.value);
   // console.log(store.lyricList.lyric);
   props.addDuration();
-  console.log(store.currentTime, "currentTime");
-  console.log(timeFilter(67392.30000001192), "11111111111111");
+  setInterval(() => {
+    nowTime.value = timeFilter(store.currentTime);
+    console.log(timeFilter(store.currentTime), "currentTime");
+  }, 1000);
 });
 const change = (target) => {
   console.log(target.target.value, "value");
+  store.currentTime = parseInt(target.target.value);
 };
 const back = () => {
   isDetailShow.value = false;
@@ -146,53 +159,52 @@ watch(
 
 <template>
   <div class="w-100% h-604px relative">
-    <div class="absolute -z-1 blur-3xl h-100% w-100% bg-red" />
-    <img class="absolute -z-1 blur-100px h-100% w-100% bg-pink brightness-50" :src="props?.musicList?.al?.picUrl" alt="">
+    <div class="absolute -z-1 blur-3xl h-100% w-100% bg-red"></div>
+    <img
+      class="absolute -z-1 blur-100px h-100% w-100% bg-pink brightness-50"
+      :src="props?.musicList?.al?.picUrl"
+      alt="背景虚化"
+    />
     <div class="flex justify-between mt-5 px-2 relative">
       <div class="text-xl text-hex-ccc" @click="back">
-        <van-icon name="arrow-left" />
+        <van-icon name="arrow-left"></van-icon>
       </div>
       <!-- 歌名跑马灯 -->
       <div class="col mr-15 py-2">
-        <Vue3Marquee class="text-sm my-2 text-hex-ccc w-30">
-          {{ props?.musicList?.name }}
-        </Vue3Marquee>
-        <!-- 艺人名字 -->
+        <Vue3Marquee class="text-sm my-2 text-hex-ccc w-30">{{
+          props?.musicList?.name
+        }}</Vue3Marquee
+        ><!-- 艺人名字 -->
         <div
+          class="flex items-center"
           v-for="(item, index) in props?.musicList?.ar"
           :key="index"
-          class="flex items-center"
         >
-          <div class="text-xs text-hex-ccc px-1">
-            {{ item.name }}
-          </div>
+          <div class="text-xs text-hex-ccc px-1">{{ item.name }}</div>
           <div class="text-xs text-hex-aaa">
-            <van-icon color="#fff" name="arrow" />
+            <van-icon color="#fff" name="arrow"></van-icon>
           </div>
         </div>
       </div>
-      <div class="text-xl text-hex-ccc">
-        <van-icon name="share-o" />
-      </div>
+      <div class="text-xl text-hex-ccc"><van-icon name="share-o"></van-icon></div>
     </div>
-    <!-- 磁盘大图 -->
-    <img
+    <!-- 磁盘大图 --><img
+      class="rounded-1/2 w-40 h-40 absolute top-30% left-25% -translate-x-1/2 animate__animated animate__bounceIn"
       v-show="!isLyricShow"
-      class=" rounded-1/2 w-40 h-40 absolute top-30% left-25% -translate-x-1/2 animate__animated animate__bounceIn"
       :src="props?.musicList?.al?.picUrl"
       alt="这是歌曲详情的磁盘图"
       @click="isLyricShow = true"
     />
     <div
+      class="musiclyricList animate__animated animate__backInDown"
       v-show="isLyricShow"
       ref="musicLyric"
-      class="musiclyricList animate__animated animate__backInDown"
       @click="isLyricShow = false"
     >
       <p
+        class="ml-6"
         v-for="item in lyric"
         :key="item"
-        class="ml-6"
         :class="{
           active:
             store.currentTime * 1000 >= item.time && store.currentTime * 1000 < item.pre,
@@ -202,53 +214,48 @@ watch(
       </p>
     </div>
     <!-- 点击展示歌词，并增加了动画效果 -->
-    <div v-show="!isLyricShow" class="flex justify-around mt-320px text-xl items-center">
-      <span><van-icon name="like-o" /></span>
-      <span style="transform: rotate(180deg)"><van-icon name="upgrade" /></span>
-      <span><van-icon name="close" /></span>
-      <span class="relative" @click="toCommentDetail"
-        ><van-icon name="comment-o" /><van-badge
-          v-if="totalComment > 0"
+    <div class="flex justify-around mt-320px text-xl items-center" v-show="!isLyricShow">
+      <span><van-icon name="like-o"></van-icon></span
+      ><span style="transform: rotate(180deg)"><van-icon name="upgrade"></van-icon></span
+      ><span><van-icon name="close"></van-icon></span
+      ><span class="relative" @click="toCommentDetail"
+        ><van-icon name="comment-o"></van-icon
+        ><van-badge
           class="absolute top-6px -right-3"
+          v-if="totalComment > 0"
           style="background: transparent; border-width: 0"
-          color="#ccc"
+          color="#ccfc"
           :content="totalComment"
           max="999"
-      /></span>
-      <span style="transform: rotate(90deg)"><van-icon name="ellipsis" /></span>
+        ></van-badge></span
+      ><span style="transform: rotate(90deg)"><van-icon name="ellipsis"></van-icon></span>
     </div>
     <div
       class="mx-2 flex mt-6 w-95% bg-transparent justify-around items-center text-xs text-hex-bbb"
     >
-      <div class="flex">00:00</div>
+      <div class="flex">{{ nowTime }}</div>
       <input
-        v-model="store.currentTime"
         class="flex justify-between mx-1"
+        v-model="store.currentTime"
         type="range"
         min="0"
         :max="store.duration"
-        step="0.05"
+        step="0.01"
         @change="change"
       />
-      <div class="flex">
-        {{ totalTime }}
-      </div>
+      <div class="flex">{{ totalTime }}</div>
     </div>
     <div class="fixed w-100% flex justify-around mt-40px text-xl items-center">
-      <div><van-icon name="replay" /></div>
-      <div @click="goPlay(-1)">
-        <van-icon name="arrow-left" />
+      <div><van-icon name="replay"></van-icon></div>
+      <div @click="goPlay(-1)"><van-icon name="arrow-left"></van-icon></div>
+      <div class="text-3xl" v-if="isShow" @click="play">
+        <van-icon name="play-circle-o"></van-icon>
       </div>
-      <div v-if="isShow" class="text-3xl" @click="play">
-        <van-icon name="play-circle-o" />
+      <div class="text-3xl" v-else @click="play">
+        <van-icon name="pause-circle-o"></van-icon>
       </div>
-      <div v-else class="text-3xl" @click="play">
-        <van-icon name="pause-circle-o" />
-      </div>
-      <div @click="goPlay(1)">
-        <van-icon name="arrow" />
-      </div>
-      <div><van-icon name="bars" /></div>
+      <div @click="goPlay(1)"><van-icon name="arrow"></van-icon></div>
+      <div><van-icon name="bars"></van-icon></div>
     </div>
   </div>
 </template>
