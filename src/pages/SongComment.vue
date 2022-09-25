4651abd5733a702e28d1037926379a4497e88f38
thosewhoyoulove <2406001659@qq.com>
2022-09-25 11:28:21 +0800

    完善了歌曲评论的样式

--- src/pages/SongComment.vue
+++ src/pages/SongComment.vue
@@ -3,76 +3,71 @@
  * @Author: 曹俊
  * @Date: 2022-08-25 12:42:09
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-09-25 11:18:17
+ * @LastEditTime: 2022-09-25 11:27:24
 -->
 <script setup lang="ts">
-import { getMusic, getMusicComment } from '~/api/SongDetail'
-const route = useRoute()
-const id = route.query.id
+import { getMusic, getMusicComment } from "~/api/SongDetail";
+const route = useRoute();
+const id = route.query.id;
 const state = reactive({
   songDetail: [], // 歌曲信息
   comment: [], // 歌曲评论
-})
-const total = ref(0) // 评论数
+});
+const total = ref(0); // 评论数
 // 控制展示最新还是最热评论
-const active = ref(0)
-const choice = ref(['最新', '最热'])
-const showLoading = ref(true)// 展示加载的图标
+const active = ref(0);
+const choice = ref(["最新", "最热"]);
+const showLoading = ref(true); // 展示加载的图标
 onMounted(async () => {
   // 获取歌曲信息
-  const res = await getMusic(id)
-  state.songDetail = res.songs
-  console.log(res, '歌曲信息')
+  const res = await getMusic(id);
+  state.songDetail = res.songs;
+  console.log(res, "歌曲信息");
   // 获取歌曲评论
-  const comment = await getMusicComment(id)
-  console.log(comment, '歌曲评论')
-  active.value = 1
-  total.value = comment.total
-  state.comment = comment.hotComments
+  const comment = await getMusicComment(id);
+  console.log(comment, "歌曲评论");
+  active.value = 1;
+  total.value = comment.total;
+  state.comment = comment.hotComments;
   setTimeout(() => {
-    showLoading.value = false
-  }, 700)
-})
+    showLoading.value = false;
+  }, 700);
+});
 const tabChange = async () => {
-  showLoading.value = true
+  showLoading.value = true;
   if (active.value == 0) {
-    state.comment = []
-    const res = await getMusicComment(id)
-    state.comment = res.comments
+    state.comment = [];
+    const res = await getMusicComment(id);
+    state.comment = res.comments;
     setTimeout(() => {
-      showLoading.value = false
-    }, 1000)
-  }
-  else if (active.value == 1) {
-    state.comment = []
-    const res = await getMusicComment(id)
-    state.comment = res.hotComments
+      showLoading.value = false;
+    }, 1000);
+  } else if (active.value == 1) {
+    state.comment = [];
+    const res = await getMusicComment(id);
+    state.comment = res.hotComments;
     setTimeout(() => {
-      showLoading.value = false
-    }, 700)
+      showLoading.value = false;
+    }, 700);
   }
-}
+};
 const filter = (num) => {
-  if (num < 10000)
-    return num
-  else if (num > 10000)
-    return `${(num / 10000).toFixed(1)}万`
-}
+  if (num < 10000) return num;
+  else if (num > 10000) return `${(num / 10000).toFixed(1)}万`;
+};
 </script>
 
 <template>
   <div class="w-100% h-100% bg-white text-sm pb-15">
-    <div class="font-700 text-left ml-5">
-      评论({{ total }})
-    </div>
-    <div class="flex break-all m-1 p-1">
+    <div class="text-left ml-5 mt-3">评论({{ total }})</div>
+    <div class="flex break-all m-1 px-1">
       <img
         class="h-4.5rem rounded-xl ml-2"
         :src="state.songDetail[0]?.al?.picUrl"
         alt="这是歌曲评论的专辑图片"
-      >
+      />
       <div class="text-left ml-2">
-        <div class="text-style text-sm font-600">
+        <div class="text-style text-xs font-600">
           {{ state.songDetail[0]?.name }}
         </div>
         <div
@@ -80,7 +75,7 @@ const filter = (num) => {
           :key="index"
           class="flex items-center"
         >
-          <div class="text-xs text-hex-ccc p-1">
+          <div class="text-xs text-hex-ccc pt-1">
             {{ item.name }}
           </div>
           <div class="text-xs text-hex-aaa">
@@ -91,9 +86,7 @@ const filter = (num) => {
     </div>
     <div>
       <van-list>
-        <div class="text-left font-600 ml-5">
-          评论区
-        </div>
+        <div class="text-left font-600 ml-5">评论区</div>
         <van-tabs v-model:active="active" @click-tab="tabChange">
           <van-tab v-for="(item, index) in choice" :key="index" :title="item">
             <div v-show="active == 0">
@@ -107,9 +100,7 @@ const filter = (num) => {
               >
                 加载中...
               </van-loading>
-              <div v-show="!state.comment.length">
-                暂无评论
-              </div>
+              <div v-show="!state.comment.length">暂无评论</div>
               <div v-show="!showLoading">
                 <ul
                   v-for="(item, index) in state.comment"
@@ -122,8 +113,8 @@ const filter = (num) => {
                         class="w-2rem h-2rem rounded-full"
                         :src="item.user?.avatarUrl"
                         alt="这是歌曲评论用户的头像"
-                      >
-                      <div class="col ml-1 text-left w-60  border-b border-hex-ddd">
+                      />
+                      <div class="col ml-2 text-left w-60 border-b border-hex-ddd">
                         <div class="text-xs">
                           {{ item?.user?.nickname }}
                         </div>
@@ -159,9 +150,7 @@ const filter = (num) => {
               >
                 加载中...
               </van-loading>
-              <div v-show="!state.comment.length">
-                暂无评论
-              </div>
+              <div v-show="!state.comment.length">暂无评论</div>
               <div v-show="!showLoading">
                 <ul
                   v-for="(item, index) in state.comment"
@@ -174,15 +163,15 @@ const filter = (num) => {
                         class="w-2rem h-2rem rounded-full"
                         :src="item.user?.avatarUrl"
                         alt="这是歌曲评论用户的头像"
-                      >
-                      <div class="col ml-1 text-left w-60  border-b  border-hex-ddd">
+                      />
+                      <div class="col ml-2 text-left w-60 border-b border-hex-ddd">
                         <div class="text-xs">
                           {{ item?.user?.nickname }}
                         </div>
                         <div class="text-0.625rem text-hex-ccc">
                           {{ item.timeStr }}
                         </div>
-                        <div class="text-.5rem my-3">
+                        <div class="text-.9rem my-3">
                           {{ item.content }}
                         </div>
                       </div>
