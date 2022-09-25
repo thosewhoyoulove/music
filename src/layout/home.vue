cac0e12b79373e7152d8c1c38c3ec2eef90c890a
thosewhoyoulove <2406001659@qq.com>
2022-09-25 10:37:26 +0800

    去除了导航栏的padding值

--- src/layout/home.vue
+++ src/layout/home.vue
@@ -3,13 +3,19 @@
  * @Author: 曹俊
  * @Date: 2022-08-18 22:50:44
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-09-18 14:23:26
+ * @LastEditTime: 2022-09-25 10:29:19
 -->
 <template>
   <van-nav-bar
-       title="网易云音乐"
+       title="云音乐"
   />
-  <div class="px-2 py-4 text-center text-gray-700 dark:text-gray-200" bg="[#f6f7f9]">
+  <div class="text-center text-gray-700 dark:text-gray-200" bg="[#f6f7f9]">
     <router-view />
   </div>
 </template>
+<style>
+.van-nav-bar__content{
+  height:2rem;
+}
+
+</style>
