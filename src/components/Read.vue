4e3f151d0c8d318dffc9eab70aa01cfb364e7034
thosewhoyoulove <2406001659@qq.com>
2022-09-25 11:00:07 +0800

    更改了声明的样式

--- src/components/Read.vue
+++ src/components/Read.vue
@@ -3,12 +3,12 @@
  * @Author: 曹俊
  * @Date: 2022-09-17 20:02:24
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-09-22 21:39:24
+ * @LastEditTime: 2022-09-25 10:59:12
 -->
 <template>
-  <div class="bg-hex-f6f7f9 w-95% pt-2">
+  <div class="bg-hex-f6f7f9 w-100vw pt-2 px-2">
     <div
-      class="bg-white p-2 h-100% border-hex-ccc rounded-lg indent text-left text-14px mb-15"
+      class="bg-white p-2 h-100% border-hex-ccc rounded-lg indent text-left text-14px mb-18"
     >
       <p>
         Hello,这个应用是移动端应用，如果你是在电脑上打开的话，请你打开开发者选项，然后点击那个手机标志，即可正常显示应用。
