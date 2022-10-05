5b1b9c04ab9cc0c88c6f73c9a2653fc5532e9922
thosewhoyoulove <2406001659@qq.com>
2022-10-05 20:40:54 +0800

    增加了歌曲MV的页面

--- src/api/MV.js
+++ src/api/MV.js
@@ -3,13 +3,42 @@
  * @Author: 曹俊
  * @Date: 2022-10-05 15:07:19
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-10-05 15:07:30
+ * @LastEditTime: 2022-10-05 20:04:47
  */
 import request from '~/utils/request'
 
+// mv 地址
+// 说明: 调用此接口, 传入 mv id, 可获取 mv 播放地址
+
+// 必选参数: id: mv id
+
+// 可选参数: r: 分辨率, 默认 1080, 可从 / mv / detail 接口获取分辨率列表
+
+// 接口地址: /mv/url
+
+// 调用例子:
+
+//     /mv/url ? id = 5436712 / mv / url ? id = 10896407 & r = 1080
 export function getMvUrl(id) {
     return request({
         method: 'post',
         url: `/mv/url?id=${id}`,
+    })
+}
+
+// 获取 mv 数据
+// 说明: 调用此接口, 传入 mvid(在搜索音乐的时候传 type = 1004 获得), 可获取对应 MV 数据, 数据包含 mv 名字, 歌手, 发布时间, mv 视频地址等数据, 其中 mv 视频 网易做了防盗链处理, 可能不能直接播放, 需要播放的话需要调用 ' mv 地址'
+// 接口
+
+// 必选参数: mvid: mv 的 id
+
+// 接口地址: /mv/detail
+
+// 调用例子: /mv/detail ? mvid = 5436712
+
+export function getMvDetail(id) {
+    return request({
+        method: 'post',
+        url: `/mv/detail?mvid=${id}`,
     })
 }
--- src/pages/MV.vue
+++ src/pages/MV.vue
@@ -3,23 +3,43 @@
  * @Author: 曹俊
  * @Date: 2022-10-05 14:58:36
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-10-05 15:13:34
+ * @LastEditTime: 2022-10-05 20:37:14
 -->
 <template>
   <div>
     <video :src="videoSrc" controls></video>
   </div>
+  <div>
+    <img class="h-10 w-10 rounded-full" :src="artistDetail?.artist?.cover" alt="" />
+    <div>{{ artistDetail?.artist?.name }}</div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { getMvUrl } from "~/api/MV";
+import { getArtistDetail } from "~/api/artist";
+import { getMvUrl, getMvDetail } from "~/api/MV";
 const route = useRoute();
-let mvId: any = parseInt(route.query.mvId);
-let videoSrc = ref()
+let mvId: any = parseInt(route.query.mvId as string);
+let videoSrc = ref();
+let mvDetail: any = ref({});
+let artistId = ref(0)
+let artistDetail:any = ref({})
 onMounted(async () => {
   let res = await getMvUrl(mvId);
   console.log(res, "res");
-  videoSrc.value = res.data.url
+  videoSrc.value = res.data.url;
+  let mvDetailRes = await getMvDetail(mvId);
+  console.log(mvDetailRes, "mvDetailRes");
+  mvDetail.value = mvDetailRes.data;
+  artistId.value = mvDetailRes.data.artistId
+  console.log(mvDetail.value,'mvDetail');
+  
+  let artistDetailRes = await getArtistDetail(artistId.value)
+  console.log(artistDetailRes,'artistDetailRes');
+  
+  artistDetail.value = artistDetailRes.data
+  console.log(artistDetail.value,'歌手详情');
+  
 });
 </script>
 
