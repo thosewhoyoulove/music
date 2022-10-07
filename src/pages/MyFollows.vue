78f16a45059dc438322f7e7b3ebfba62d91925ee
thosewhoyoulove <2406001659@qq.com>
2022-10-07 20:49:39 +0800

    点击用户的关注列表但是跳转不到歌手主页

--- src/pages/MyFollows.vue
+++ src/pages/MyFollows.vue
@@ -3,77 +3,73 @@
  * @Author: 曹俊
  * @Date: 2022-09-15 15:07:05
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-09-16 19:46:21
+ * @LastEditTime: 2022-10-07 20:26:51
 -->
 <script setup lang="ts">
-import { getUserFolloweds, getUserFollows } from '~/api/user'
-const route = useRoute()
-const uid = parseInt(route.query.id)
-const choice = ref(['关注', '粉丝'])
-const active = ref(Number(route.query.active))
-const followsList = ref([])
-const followedList = ref([])
+import { getUserFolloweds, getUserFollows } from "~/api/user";
+const route = useRoute();
+const router = useRouter()
+const uid = parseInt(route.query.id as string);
+const choice = ref(["关注", "粉丝"]);
+const active = ref(Number(route.query.active));
+const followsList:any = ref([]);
+const followedList:any = ref([]);
 onMounted(async () => {
   if (active.value == 1) {
-    const res = await getUserFolloweds(uid)
-    console.log(res, '粉丝列表')
-    followedList.value = res.followeds
+    const res = await getUserFolloweds(uid);
+    console.log(res, "粉丝列表");
+    followedList.value = res.followeds;
+  } else if (active.value == 0) {
+    const res = await getUserFollows(uid);
+    console.log(res, "关注列表");
+    followsList.value = res.follow;
   }
-  else if (active.value == 0) {
-    const res = await getUserFollows(uid)
-    console.log(res, '关注列表')
-    followsList.value = res.follow
-  }
-  console.log(uid, 'uid')
-})
+  console.log(uid, "uid");
+});
 const tabChange = async () => {
   if (active.value == 1) {
-    const res = await getUserFolloweds(uid)
-    console.log(res, '粉丝列表')
-    followedList.value = res.followeds
+    const res = await getUserFolloweds(uid);
+    console.log(res, "粉丝列表");
+    followedList.value = res.followeds;
+  } else if (active.value == 0) {
+    const res = await getUserFollows(uid);
+    console.log(res, "关注列表");
+    followsList.value = res.follow;
   }
-  else if (active.value == 0) {
-    const res = await getUserFollows(uid)
-    console.log(res, '关注列表')
-    followsList.value = res.follow
-  }
-}
+};
+//跳转歌手主页
+const toArtistDetail = (item:any) => {
+  console.log(item);
+  router.push({
+    path: "/Artist",
+    query: {
+      artistId: item.userId
+    },
+  });
+};
 </script>
 
 <template>
   <div>
-    <van-tabs v-model:active="active" @click-tab="tabChange">
+    <van-tabs v-model:active="active" @click-tab="tabChange" class="pb-20">
       <van-tab v-for="(item, index) in choice" :key="index" :title="item">
         <div
           v-for="(item, index) in followsList"
           v-show="active == 0"
           :key="index"
           class="flex w-100%"
+          @click="toArtistDetail(item)"
         >
           <div class="relative">
-            <img
-              class="rounded-full w-11 h-11 m-2"
-              :src="item.avatarUrl"
-              alt=""
-            >
+            <img class="rounded-full w-11 h-11 m-2" :src="item.avatarUrl" alt="" />
             <img
               v-if="item?.avatarDetail?.identityIconUrl"
               class="w-5 h-5 absolute rounded-full left-10 bottom-1 z-10"
               :src="item?.avatarDetail?.identityIconUrl"
               alt=""
-            >
+            />
           </div>
-          <div
-            class="
-              col
-              h-10
-              w-15.625rem
-              text-left
-              ml-3
-              mt-3
-              border-b border-hex-ccc
-            "
-          >
+          <div class="col h-10 w-15.625rem text-left ml-3 mt-3">
             <div class="text-sm">
               {{ item.nickname }}
             </div>
@@ -89,29 +85,15 @@ const tabChange = async () => {
           class="flex w-100%"
         >
           <div class="relative">
-            <img
-              class="rounded-full w-11 h-11 m-2"
-              :src="item.avatarUrl"
-              alt=""
-            >
+            <img class="rounded-full w-11 h-11 m-2" :src="item.avatarUrl" alt="" />
             <img
               v-if="item?.avatarDetail?.identityIconUrl"
               class="w-5 h-5 absolute rounded-full left-10 bottom-1 z-10"
               :src="item?.avatarDetail?.identityIconUrl"
               alt=""
-            >
+            />
           </div>
-          <div
-            class="
-              col
-              h-10
-              w-15.625rem
-              text-left
-              ml-3
-              mt-3
-              border-b border-hex-ccc
-            "
-          >
+          <div class="col h-10 w-15.625rem text-left ml-3 mt-3">
             <div class="text-sm">
               {{ item.nickname }}
             </div>
