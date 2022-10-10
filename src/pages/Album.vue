eef91ff0f41423b17ae34e198968961ce5630daa
thosewhoyoulove <2406001659@qq.com>
2022-10-10 12:34:15 +0800

    专辑详情跳转歌手页

--- src/pages/Album.vue
+++ src/pages/Album.vue
@@ -3,8 +3,79 @@
  * @Author: 曹俊
  * @Date: 2022-10-09 21:08:11
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-10-09 21:56:54
+ * @LastEditTime: 2022-10-10 12:32:46
 -->
+
+<script setup lang="ts">
+import { getAlbumDetail } from "~/api/album";
+import { useStore } from "~/store/index";
+const store = useStore();
+const route = useRoute();
+const router = useRouter();
+let albumId = parseInt(route.query.albumId as string);
+let state: any = reactive({
+  album: {},
+  songs: [],
+});
+onMounted(async () => {
+  console.log(albumId, "接收专辑id ");
+
+  let albumRes = await getAlbumDetail(albumId);
+  state.album = albumRes.album;
+  state.songs = albumRes.songs;
+  console.log(albumRes, "专辑的内容");
+});
+const filterTotal = (num: any) => {
+  if (num > 1000000) return "100w+";
+  else if (num > 100000) return "10w+";
+  else if (num > 10000) return "1w+";
+  else if (num > 1000) return "999+";
+  else return num;
+};
+const toMv = (item: any) => {
+  console.log(item);
+  router.push({
+    path: "MV",
+    query: {
+      mvId: item.mv,
+      artistId: item.ar[0].id,
+    },
+  });
+};
+// 修改歌曲信息并进行播放
+const updateSongList = (index: any) => {
+  store.updatePlayList(store.$state, state.songs); // 将歌单列表传进默认列表
+  store.updatePlayListIndex(index); // 将索引值传给默认索引
+  store.updateIsShow(store.$state, true); // 修改为暂停图标
+};
+//格式化专辑出版时间
+const addZero = (num: any) => {
+  if (parseInt(num) < 10) num = `0${num}`;
+  return num;
+};
+const formatMsToDate = (ms: any) => {
+  if (ms) {
+    const oDate = new Date(ms);
+    const oYear = oDate.getFullYear();
+    const oMonth = oDate.getMonth() + 1;
+    const oDay = oDate.getDay();
+    const oTime = `${oYear}.${addZero(oMonth)}.${addZero(oDay)}`;
+    return oTime;
+  } else {
+    return "";
+  }
+};
+//跳转歌手主页
+const toArtistDetail = (id:any) => {
+  console.log(id);
+  router.push({
+    path: "/Artist",
+    query: {
+      artistId: id,
+    },
+  });
+};
+</script>
 <template>
   <div class="bg-white w-100vw h-42vh relative">
     <img
@@ -71,7 +142,7 @@
         <div class="flex items-center">
           <van-icon size="1rem" name="add" color="#FE3641" />
           <div class="px-1 text-10px">
-            {{ filterTotal(state?.album?.subscribedCount) }}
+           null
           </div>
         </div>
       </div>
@@ -144,76 +215,5 @@
     </van-list>
   </div>
 </template>
-
-<script setup lang="ts">
-import { getAlbumDetail } from "~/api/album";
-import { useStore } from "~/store/index";
-const store = useStore();
-const route = useRoute();
-const router = useRouter();
-let albumId = parseInt(route.query.albumId as string);
-let state: any = reactive({
-  album: {},
-  songs: [],
-});
-onMounted(async () => {
-  console.log(albumId, "接收专辑id ");
-
-  let albumRes = await getAlbumDetail(albumId);
-  state.album = albumRes.album;
-  state.songs = albumRes.songs;
-  console.log(albumRes, "专辑的内容");
-});
-const filterTotal = (num: any) => {
-  if (num > 1000000) return "100w+";
-  else if (num > 100000) return "10w+";
-  else if (num > 10000) return "1w+";
-  else if (num > 1000) return "999+";
-  else return num;
-};
-const toMv = (item: any) => {
-  console.log(item);
-  router.push({
-    path: "MV",
-    query: {
-      mvId: item.mv,
-      artistId: item.ar[0].id,
-    },
-  });
-};
-// 修改歌曲信息并进行播放
-const updateSongList = (index: any) => {
-  store.updatePlayList(store.$state, state.songs); // 将歌单列表传进默认列表
-  store.updatePlayListIndex(index); // 将索引值传给默认索引
-  store.updateIsShow(store.$state, true); // 修改为暂停图标
-};
-//格式化专辑出版时间
-const addZero = (num: any) => {
-  if (parseInt(num) < 10) num = `0${num}`;
-  return num;
-};
-const formatMsToDate = (ms: any) => {
-  if (ms) {
-    const oDate = new Date(ms);
-    const oYear = oDate.getFullYear();
-    const oMonth = oDate.getMonth() + 1;
-    const oDay = oDate.getDay();
-    const oTime = `${oYear}.${addZero(oMonth)}.${addZero(oDay)}`;
-    return oTime;
-  } else {
-    return "";
-  }
-};
-//跳转歌手主页
-const toArtistDetail = (state.album.artists[0].id) => {
-  console.log(state.album.artists[0].id);
-  router.push({
-    path: "/Artist",
-    query: {
-      artistId: state.album.artists[0].id,
-    },
-  });
-};
-</script>
 
 <style scoped></style>
