1ce01234c4576df9057826adef0a5f76495cef0b
thosewhoyoulove <2406001659@qq.com>
2022-10-10 16:34:45 +0800

    专辑详情接口

--- src/api/SongListDetail.js
+++ src/api/SongListDetail.js
@@ -3,7 +3,7 @@
  * @Author: 曹俊
  * @Date: 2022-08-17 15:53:02
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-10-07 20:54:50
+ * @LastEditTime: 2022-10-10 12:57:25
  */
 import request from '~/utils/request'
 //获取歌单详情 (包括推荐歌单和热门歌单)
@@ -39,9 +39,9 @@ export function getAllSong(data, limit, offset = 0) {
 // offset: 偏移数量 , 用于分页 , 如 :( 评论页数 -1)*20, 其中 20 为 limit 的值
 
 // before: 分页参数,取上一页最后一项的 time 获取下一页数据(获取超过 5000 条评论的时候需要用到)
-export function getSongListComment(data, limit, offset = 0) {
+export function getSongListComment(id, limit, offset = 0) {
     return request({
         method: 'post',
-        url: `/comment/playlist?id=${data}&limit=${limit}&offset=${offset}` //搜索的是100条，但渲染的值是热门评论，只有15条
+        url: `/comment/playlist?id=${id}&limit=${limit}&offset=${offset}` //搜索的是100条，但渲染的值是热门评论，只有15条
     })
 }
--- src/api/album.js
+++ src/api/album.js
@@ -3,7 +3,7 @@
  * @Author: 曹俊
  * @Date: 2022-10-09 21:11:29
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-10-09 21:11:36
+ * @LastEditTime: 2022-10-10 12:58:05
  */
 // 获取专辑内容
 // 说明: 调用此接口, 传入专辑 id, 可获得专辑内容
@@ -19,5 +19,25 @@ export function getAlbumDetail(id) {
     return request({
         method: 'post',
         url: `album?id=${id}`,
+    })
+}
+// 专辑评论
+// 说明: 调用此接口, 传入音乐 id 和 limit 参数, 可获得该专辑的所有评论(不需要 登录)
+
+// 必选参数: id: 专辑 id
+
+// 可选参数: limit: 取出评论数量, 默认为 20
+
+// offset: 偏移数量, 用于分页, 如: (评论页数 - 1) * 20, 其中 20 为 limit 的值
+
+// before: 分页参数, 取上一页最后一项的 time 获取下一页数据(获取超过 5000 条评论的时候需要用到)
+
+// 接口地址: /comment/album
+
+// 调用例子: /comment/album ? id = 32311
+export function getAlbumComment(id, limit, offset = 0) {
+    return request({
+        method: 'post',
+        url: `comment/album?id=${id}&limit=${limit}&offset=${offset}`,
     })
 }
