f79fad0a3f9ee9df7d4267eed62193b86420419c
thosewhoyoulove <2406001659@qq.com>
2022-10-12 21:31:07 +0800

    引入了lodash库的防抖函数

--- components.d.ts
+++ components.d.ts
@@ -27,6 +27,7 @@ declare module '@vue/runtime-core' {
     VanCircle: typeof import('vant/es')['Circle']
     VanCollapse: typeof import('vant/es')['Collapse']
     VanCollapseItem: typeof import('vant/es')['CollapseItem']
+    VanDialog: typeof import('vant/es')['Dialog']
     VanDivider: typeof import('vant/es')['Divider']
     VanField: typeof import('vant/es')['Field']
     VanForm: typeof import('vant/es')['Form']
