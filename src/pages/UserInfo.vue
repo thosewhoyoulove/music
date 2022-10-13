c772b97591aa07aa04eefc0d67e155916aeaa7ed
thosewhoyoulove <2406001659@qq.com>
2022-10-13 16:54:09 +0800

    增加了修改性别的功能

--- src/pages/UserInfo.vue
+++ src/pages/UserInfo.vue
@@ -3,21 +3,21 @@
  * @Author: 曹俊
  * @Date: 2022-10-11 16:12:27
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-10-13 15:35:37
+ * @LastEditTime: 2022-10-13 16:47:19
 -->
 <template>
   <div class="w-100vw h-100vh bg-hex-eee">
     <div class="text-hex-eee items-center">
       <van-cell-group class="title" inset title="我的资料">
-        <van-cell  @click="toEditInfo((flag = 0))" class="cell" v-model:value="nickname" title="昵称"
+        <van-cell
+          @click="toEditInfo((flag = 0))"
+          class="cell"
+          v-model:value="nickname"
+          title="昵称"
           ><template #right-icon>
-            <van-icon
-              name="edit"
-              class="flex item-center"
-             
-            /> </template
+            <van-icon name="edit" class="flex item-center" /> </template
         ></van-cell>
-        <van-cell v-model:value="gender" title="性别" />
+        <van-cell @click="showCheckBox = true" v-model:value="gender" title="性别" />
         <van-cell @click="showCalendar = true" v-model:value="birthday" title="生日" />
         <van-cell
           @click="toEditInfo((flag = 1))"
@@ -34,9 +34,31 @@
     :min-date="minDate"
     :max-date="maxDate"
   />
+  <van-overlay class="overlay" :show="showCheckBox" @click="showCheckBox = false">
+    <div class="flex items-center justify-center w-100vw h-100vh">
+      <van-radio-group v-model="gender">
+        <van-cell-group
+          class="flex-col items-center justify-between text-left w-90vw h-15vh"
+          inset
+        >
+          <van-cell title="男" clickable @click="checkGender(gender)">
+            <template #right-icon>
+              <van-radio name="男" />
+            </template>
+          </van-cell>
+          <van-cell title="女" clickable @click="checkGender(gender)">
+            <template #right-icon>
+              <van-radio name="女" />
+            </template>
+          </van-cell>
+        </van-cell-group>
+      </van-radio-group>
+    </div>
+  </van-overlay>
 </template>
 
 <script setup lang="ts">
+import { Notify } from "vant";
 import { getUserAcount, updateUser } from "~/api/user";
 const router = useRouter();
 const nickname = ref("");
@@ -48,6 +70,7 @@ let profile: any = ref({});
 const minDate = new Date(1970, 0, 1);
 const maxDate = new Date(2024, 0, 1);
 const showCalendar = ref(false); //日历组件的展示
+const showCheckBox = ref(false);
 const formatDate = (birthday: any) =>
   `${birthday.getFullYear()}-${birthday.getMonth() + 1}-${birthday.getDate()}`; //将选择值转化为yy-mm-dd
 //修改生日
@@ -86,6 +109,36 @@ onMounted(async () => {
   birthday.value = formatMsToDate(profile.value.birthday);
   signature.value = profile.value.signature;
 });
+//选择性别
+const checkGender = async (gender: any) => {
+  console.log(gender);
+  let infoRes = await getUserAcount();
+  let oldGender = infoRes.profile.gender;
+  birthday.value = infoRes.profile.birthday;
+  if (oldGender == gender) {
+    showCheckBox.value = false;
+    Notify({ type: "success", message: "修改成功" });
+  } else {
+    if (gender == "男") {
+      gender = 1;
+      let res = await updateUser(nickname.value, gender, birthday.value, signature.value);
+      if (res.code !== 200) {
+        Notify({ type: "warning", message: res.message });
+      } else if (res.code === 200) {
+        Notify({ type: "success", message: "修改成功" });
+      }
+    } else if (gender == "女") {
+      gender = 2;
+      let res = await updateUser(nickname.value, gender, birthday.value, signature.value);
+      if (res.code !== 200) {
+        Notify({ type: "warning", message: res.message });
+      } else if (res.code === 200) {
+        Notify({ type: "success", message: "修改成功" });
+      }
+    }
+  }
+  birthday.value = formatDate(birthday.value)
+};
 //生日时间
 const addZero = (num: any) => {
   if (parseInt(num) < 10) num = `0${num}`;
@@ -104,6 +157,7 @@ const formatMsToDate = (ms: any) => {
     return "";
   }
 };
+
 const toEditInfo = (flag: any) => {
   router.push({
     path: "/EditInfo",
@@ -120,5 +174,9 @@ const toEditInfo = (flag: any) => {
 }
 :deep(.cell) {
   align-items: center;
+}
+
+:deep(.overlay) {
+  background-color: rgba(0, 0, 0, 0.3);
 }
 </style>
