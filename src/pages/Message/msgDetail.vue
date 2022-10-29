7234288cf0679047b03fffc0c3250572411fa53c
thosewhoyoulove <2406001659@qq.com>
2022-10-29 15:42:17 +0800

    私信详情增加了一个聊天框

--- src/pages/Message/msgDetail.vue
+++ src/pages/Message/msgDetail.vue
@@ -6,15 +6,15 @@
  * @LastEditTime: 2022-10-28 21:06:00
 -->
 <template>
-  <div class="w-100vw bg-hex-ddd" style="min-height: calc(100vh - 100px)">
+  <div class="bg-hex-ddd" style="min-height: calc(100vh - 1px)">
     <van-list
       v-model:loading="listLoading"
       :finished="finished"
       @load="onLoad"
-      direction="up"
-      class="p-2 pb-20"
+      direction="down"
+      class="p-2 pb-20 flex flex-col-reverse overflow-scroll"
     >
-      <div v-for="(item, index) in msgs" :key="index" class="flex-col-reverse w-100%">
+      <div v-for="(item, index) in msgs" :key="index" class="w-100%">
         <div class="scale-50">{{ formatMsToDate(item.time) }}</div>
         <div :class="[item.fromUser.userId == id ? 'avatar-right' : 'avatar-left']">
           <img class="w-7 h-7 rounded-full" :src="item.fromUser.avatarUrl" alt="" />
@@ -28,6 +28,7 @@
               background-color: rgba(255, 255, 255, var(--tw-bg-opacity));
               border-radius: 0.5rem;
               width: 77vw;
+              margin-bottom: 1.5rem;
             "
             :class="[item.fromUser.userId == id ? 'word-right' : 'word-left']"
           >
@@ -41,9 +42,14 @@
         </div>
       </div></van-list
     >
-    <div class="fixed mr-1.7 right-4px bottom-40px" @click="toBottom">
+    <div class="fixed mr-1.7 right-4px bottom-42px" @click="toBottom">
       <van-icon size="10px" name="arrow-down" />
     </div>
+  </div>
+  <div class="bg-hex-fff h-8vh fixed bottom-0 w-100% flex justify-between items-center">
+    <van-field v-model="sendMsg" clearable placeholder="发送消息" />
+    <van-icon class="mx-2" name="add-o" />
+    <div class="text-left text-xs flex w-10">发送</div>
   </div>
 </template>
 
@@ -57,20 +63,26 @@ const { isFooterShow } = storeToRefs(store);
 const route = useRoute();
 let uid = parseInt(route.query.uid as any);
 let id = ref(0);
-let limit = ref(30);
+let limit = ref(10);
 let msgs: any = ref([]); //所有的消息数组
 let more: any = ref(false);
 const listLoading = ref(false); //下拉刷新加载提示
 const finished = ref(false); //是否结束
+let sendMsg = ref("");
 onMounted(async () => {
+  isFooterShow.value = false;
+
   let res = await getUserAcount();
   console.log(res, "用户信息");
   id.value = res.profile.userId;
-  isFooterShow.value = false;
+  let res2 = await getMsgDetail(uid, limit.value);
   window.scrollTo({
     top: Math.max(document.body.scrollHeight, document.documentElement.scrollHeight),
     behavior: "smooth",
   });
+  console.log(JSON.parse(res2.msgs[0].msg).msg);
+  console.log(res2, "新加载的");
+  msgs.value = res2.msgs;
 });
 //注册时间
 const addZero = (num: any) => {
@@ -101,7 +113,7 @@ const onLoad = async () => {
   let res = await getMsgDetail(uid, limit.value);
   console.log(JSON.parse(res.msgs[0].msg).msg);
   console.log(res, "新加载的");
-  msgs.value = res.msgs.reverse();
+  msgs.value = res.msgs;
   more.value = res.more;
   listLoading.value = false;
   if (more.value == false) {
