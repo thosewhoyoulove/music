43fd51912c686a78fe8d98ebfb0d87cfee8ee073
thosewhoyoulove <2406001659@qq.com>
2022-10-31 14:50:14 +0800

    完善了私信详情的样式以及功能

--- src/pages/Message/msgDetail.vue
+++ src/pages/Message/msgDetail.vue
@@ -3,29 +3,34 @@
  * @Author: 曹俊
  * @Date: 2022-10-28 16:18:24
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-10-28 21:06:00
+ * @LastEditTime: 2022-10-31 14:48:34
 -->
 <template>
-  <div class="bg-hex-ddd" style="min-height: calc(100vh - 8vh)">
+  <div class="bg-hex-F5F5F5" style="height: 100vh">
     <van-list
       v-model:loading="listLoading"
       :finished="finished"
       @load="onLoad"
       direction="down"
       :immediate-check="false"
-      class="p-2 flex flex-col-reverse overflow-scroll h-95vh py-20"
+      id="listContainer"
+      class="px-2 flex flex-col-reverse overflow-scroll h-92vh pt-20"
     >
       <div v-for="(item, index) in msgs" :key="index" class="w-100%">
         <div class="scale-50">{{ formatMsToDate(item.time) }}</div>
         <div :class="[item?.fromUser?.userId == id ? 'avatar-right' : 'avatar-left']">
-          <img class="w-7 h-7 rounded-full" :src="item?.fromUser?.avatarUrl" alt="" />
+          <img
+            @click="toArtistDetail(item)"
+            class="w-7 h-7 rounded-full"
+            :src="item?.fromUser?.avatarUrl"
+            alt=""
+          />
           <div
             style="
               position: relative;
               font-size: 0.75rem;
               word-break: break-all;
               --tw-bg-opacity: 1;
-              background-color: rgba(255, 255, 255, var(--tw-bg-opacity));
               border-radius: 0.5rem;
               width: auto;
               max-width: 77vw;
@@ -44,21 +49,27 @@
         </div>
       </div></van-list
     >
-    <!-- <div class="fixed mr-1.7 right-4px bottom-42px" @click="toBottom">
-      <van-icon size="10px" name="arrow-down" />
-    </div> -->
   </div>
   <div class="bg-hex-fff h-8vh fixed bottom-0 w-100% flex justify-between items-center">
     <van-field
+      @focus="toBottom"
       v-model="message"
       clearable
       placeholder="发送消息"
       @keyup.enter="sendAMessage(message, uid)"
-    />
-    <van-icon class="mx-2" name="add-o" />
-    <div @click.enter="sendAMessage(message, uid)" class="text-left text-xs flex w-10">
-      发送
-    </div>
+    >
+      <template class="flex items-center" #button>
+        <van-icon class="mx-2 flex items-center" name="add-o" />
+        <van-button
+          @click="sendAMessage(message, uid)"
+          class="flex items-center"
+          :disabled="disabled"
+          size="mini"
+          :hairline="false"
+          >发送</van-button
+        >
+      </template></van-field
+    >
   </div>
 </template>
 
@@ -70,28 +81,27 @@ import { getMsgDetail, sendMsg } from "~/api/message";
 const store = useStore();
 const { isFooterShow } = storeToRefs(store);
 const route = useRoute();
+const router = useRouter();
 let uid = parseInt(route.query.uid as any); //对方id
 //通过判断自己的id与发消息的id是否一致来判断左边还是右边
+let disabled = ref(true);
 let id = ref(0); //自己id
-let limit = ref(5);
+let limit = ref(30);
 let msgs: any = ref([] as any[]); //所有的消息数组
 let more: any = ref(false);
 const listLoading = ref(false); //下拉刷新加载提示
 const finished = ref(false); //是否结束
 let message = ref("");
-
+let listContainer: any = null; //获取van-list的元素对象，然后用来跳转到最底部
 onMounted(async () => {
   isFooterShow.value = false;
+  listContainer = document.getElementById("listContainer");
 
   let res = await getUserAcount();
   console.log(res, "用户信息");
   id.value = res.profile.userId;
 
   let res2 = await getMsgDetail(uid, limit.value);
-  // window.scrollTo({
-  //   top: Math.max(document.body.scrollHeight, document.documentElement.scrollHeight),
-  //   behavior: "smooth",
-  // });
   console.log(JSON.parse(res2.msgs[0].msg).msg);
   console.log(res2, "新加载的");
   msgs.value = res2.msgs;
@@ -108,7 +118,7 @@ const formatMsToDate = (ms: any) => {
     const oDate = new Date(ms);
     const oYear = oDate.getFullYear();
     const oMonth = oDate.getMonth() + 1;
-    const oDay = oDate.getDay();
+    const oDay = oDate.getDate();
     const oHour = oDate.getHours();
     const oMinute = oDate.getMinutes();
     const oTime = `${oYear}年${addZero(oMonth)}月${addZero(oDay)}日 ${addZero(
@@ -133,15 +143,8 @@ const onLoad = async () => {
     finished.value = true;
   }
 };
-// const toBottom = () => {
-//   console.log(111);
-
-//   window.scrollTo({
-//     top: Math.max(document.body.scrollHeight, document.documentElement.scrollHeight),
-//     behavior: "smooth",
-//   });
-// };
 const sendAMessage = async (msg: any, uid: any) => {
+  toBottom();
   message.value = "";
   console.log(msg, uid);
   let res = await sendMsg(uid, msg);
@@ -153,6 +156,35 @@ const sendAMessage = async (msg: any, uid: any) => {
   });
   console.log(msgs.value);
 };
+//跳转歌手主页,跳转不了，没有歌手id，只有歌手的userId
+const toArtistDetail = (item: any) => {
+  console.log(item);
+  // router.push({
+  //   path: "/Artist",
+  //   query: {
+  //     artistId: item.fromUser.userId,
+  //   },
+  // });
+};
+//filed获取焦点以及发送消息的时候跳转到最底部
+const toBottom = () => {
+  listContainer.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+watch(
+  () => message.value,
+  (newVal) => {
+    if (newVal.length !== 0) {
+      disabled.value = false;
+      console.log(disabled.value);
+    } else {
+      disabled.value = true;
+    }
+  }
+);
 </script>
 
 <style lang="css" scoped>
@@ -177,7 +209,7 @@ const sendAMessage = async (msg: any, uid: any) => {
   right: -8px;
   border-width: 4px;
   border-style: solid;
-  border-color: transparent transparent transparent#fff;
+  border-color: transparent transparent transparent#6495ED;
 }
 .avatar-left {
   display: flex;
@@ -189,9 +221,19 @@ const sendAMessage = async (msg: any, uid: any) => {
 .word-left {
   text-align: left;
   margin-left: 0.5rem;
+  background-color: rgba(255, 255, 255, var(--tw-bg-opacity));
 }
 .word-right {
+  color: #fff;
   text-align: right;
   margin-right: 0.5rem;
+  background-color: rgba(100, 149, 237, var(--tw-bg-opacity));
+}
+:deep(.van-field__button) {
+  display: flex;
+  align-items: center;
+}
+:deep(.van-button--default) {
+  border: 0 solid #fff;
 }
 </style>
