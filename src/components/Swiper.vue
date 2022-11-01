4a5b120b226d83abcf13353094a489edd1b003fd
thosewhoyoulove <2406001659@qq.com>
2022-11-01 16:32:07 +0800

    准备修改首页轮播图

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -6,39 +6,30 @@
  * @LastEditTime: 2022-10-04 16:30:27
 -->
 <script setup lang="ts">
-import { onMounted, reactive } from 'vue'
-import { getBanner } from '~/api/banner.js'
-const loading = ref(true)
+import { onMounted, reactive } from "vue";
+import { getBanner } from "~/api/banner.js";
+const loading = ref(true);
 const state = reactive({
-  image: [],
-})
+  image: [] as any[],
+});
 onMounted(async () => {
-  const res = await getBanner()
-  console.log(res,'res');
-  
-  state.image = res.banners
-  loading.value = false
-})
+  const res = await getBanner();
+  console.log(res, "res");
+
+  state.image = res.banners;
+  loading.value = false;
+});
 </script>
 
 <template>
   <div class="pt-2 w-100vw px-2">
     <van-skeleton :row="4" round :loading="loading" />
-    <van-swipe  class="rounded-xl" :autoplay="3000" lazy-render>
-      <van-swipe-item
-        v-for="image in state.image"
-        :key="image"
-        class=""
-      >
-        <img
-          class="h-30 w-100vw rounded-xl"
-          :src="image.pic"
-          alt="这是首页轮播图"
-        >
+    <van-swipe class="rounded-xl" :autoplay="3000" lazy-render>
+      <van-swipe-item v-for="image in state.image" :key="image" class="">
+        <img class="h-30 w-100vw rounded-xl" :src="image.pic" alt="这是首页轮播图" />
       </van-swipe-item>
     </van-swipe>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
