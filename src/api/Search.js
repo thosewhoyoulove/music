2d9c0b3f2db4f4a552bbce0f139c18f01b47e4e4
thosewhoyoulove <2406001659@qq.com>
2022-11-01 20:52:01 +0800

    搜索接口加了limit参数

--- src/api/Search.js
+++ src/api/Search.js
@@ -13,10 +13,10 @@
 // type: 搜索类型；默认为 1 即单曲 , 取值意义 : 1: 单曲, 10: 专辑, 100: 歌手, 1000: 歌单, 1002: 用户, 1004: MV, 1006: 歌词, 1009: 电台, 1014: 视频, 1018:综合, 2000:声音(搜索声音返回字段格式会不一样)
 import request from '~/utils/request'
 
-export function getCloudSearch(keywords, type) {
+export function getCloudSearch(keywords, type, limit) {
     return request({
         method: 'get',
-        url: `/cloudsearch?keywords=${keywords}&type=${type}`,
+        url: `/cloudsearch?keywords=${keywords}&type=${type}&limit=${limit}`,
     })
 }
 //用于搜索不同的类型
